2854150a1646b8e589855b54d781e1be5157c2e9
kimstart <rladbtjq18@naver.com>
2021-08-06 16:26:12 +0900

    엑셀출력 진행중

--- s20210704/src/main/java/com/oracle/s20210704/controller/YsController.java
+++ s20210704/src/main/java/com/oracle/s20210704/controller/YsController.java
@@ -13,6 +13,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.util.HSSFColor.HSSFColorPredefined;
+import org.apache.poi.ss.usermodel.BorderStyle;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -518,5 +528,127 @@ public class YsController {
 			file.delete();
 		}
 	}
+  	@GetMapping(value = "cmt/excelDown")
+
+  	public void excelDown(HttpServletResponse response, YsEmpCmt ysEmpCmt,String currentPage) throws Exception {
+  	    // 목록조회
+  		int cmtTotal = yecs.cmtTotal();
+  		YsPaging yp = new YsPaging(cmtTotal, currentPage);
+		ysEmpCmt.setStart(yp.getStart());
+		ysEmpCmt.setEnd(yp.getEnd());
+  		List<YsEmpCmt> cmtList = yecs.cmtList(ysEmpCmt);
+
+  	    // 워크북 생성
+
+  	    Workbook wb = new HSSFWorkbook();
+  	    Sheet sheet = wb.createSheet("근태 목록");
+  	    Row row = null;
+  	    Cell cell = null;
+  	    int rowNo = 0;
+
+
+  	    // 테이블 헤더용 스타일
+  	    CellStyle headStyle = wb.createCellStyle();
+
+  	    // 가는 경계선을 가집니다.
+  	    headStyle.setBorderTop(BorderStyle.THIN);
+  	    headStyle.setBorderBottom(BorderStyle.THIN);
+  	    headStyle.setBorderLeft(BorderStyle.THIN);
+  	    headStyle.setBorderRight(BorderStyle.THIN);
+
+  	    // 배경색은 노란색입니다.
+  	    headStyle.setFillForegroundColor(HSSFColorPredefined.YELLOW.getIndex());
+  	    headStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+
+
+  	    // 데이터는 가운데 정렬합니다.
+  	    headStyle.setAlignment(HorizontalAlignment.CENTER);
+
+  	    // 데이터용 경계 스타일 테두리만 지정
+  	    CellStyle bodyStyle = wb.createCellStyle();
+  	    bodyStyle.setBorderTop(BorderStyle.THIN);
+  	    bodyStyle.setBorderBottom(BorderStyle.THIN);
+  	    bodyStyle.setBorderLeft(BorderStyle.THIN);
+  	    bodyStyle.setBorderRight(BorderStyle.THIN);
+
+  	    // 헤더 생성
+  	    row = sheet.createRow(rowNo++);
+  	    cell = row.createCell(0);
+  	    cell.setCellStyle(headStyle);
+  	    cell.setCellValue("사원번호");
+  	    cell = row.createCell(1);
+  	    cell.setCellStyle(headStyle);
+  	    cell.setCellValue("이름");
+  	    cell = row.createCell(2);
+  	    cell.setCellStyle(headStyle);
+  	    cell.setCellValue("부서");
+  	    cell = row.createCell(3);
+	    cell.setCellStyle(headStyle);
+	    cell.setCellValue("소속");
+	    cell = row.createCell(4);
+  	    cell.setCellStyle(headStyle);
+  	    cell.setCellValue("직급");
+  	    cell = row.createCell(5);
+	    cell.setCellStyle(headStyle);
+	    cell.setCellValue("출근시간");
+	    cell = row.createCell(6);
+  	    cell.setCellStyle(headStyle);
+  	    cell.setCellValue("퇴근시간");
+  	    cell = row.createCell(7);
+	    cell.setCellStyle(headStyle);
+	    cell.setCellValue("날짜");
+	    cell = row.createCell(8);
+  	    cell.setCellStyle(headStyle);
+  	    cell.setCellValue("상태");
+  	    cell = row.createCell(9);
+	    cell.setCellStyle(headStyle);
+	    cell.setCellValue("수정일");
+
+  	    // 데이터 부분 생성
+  	    for(YsEmpCmt cmt : cmtList) {
+  	        row = sheet.createRow(rowNo++);
+  	        cell = row.createCell(0);
+  	        cell.setCellStyle(bodyStyle);
+  	        cell.setCellValue(cmt.getEmp_num());
+  	        cell = row.createCell(1);
+  	        cell.setCellStyle(bodyStyle);
+  	        cell.setCellValue(cmt.getEmp_name());
+  	        cell = row.createCell(2);
+  	        cell.setCellStyle(bodyStyle);
+  	        cell.setCellValue(cmt.getDcontent());
+  	        cell = row.createCell(3);
+	        cell.setCellStyle(bodyStyle);
+	        cell.setCellValue(cmt.getTcontent());
+	        cell = row.createCell(4);
+  	        cell.setCellStyle(bodyStyle);
+  	        cell.setCellValue(cmt.getRcontent());
+  	        cell = row.createCell(5);
+	        cell.setCellStyle(bodyStyle);
+	        cell.setCellValue(cmt.getSrttime());
+	        cell = row.createCell(6);
+  	        cell.setCellStyle(bodyStyle);
+  	        cell.setCellValue(cmt.getEndtime());
+  	        cell = row.createCell(7);
+	        cell.setCellStyle(bodyStyle);
+	        cell.setCellValue(cmt.getCmt_date());
+	        cell = row.createCell(8);
+  	        cell.setCellStyle(bodyStyle);
+  	        cell.setCellValue("상태");
+  	        cell = row.createCell(9);
+	        cell.setCellStyle(bodyStyle);
+	        cell.setCellValue(cmt.getCmt_md());
+
+  	    }
+
+  	    // 컨텐츠 타입과 파일명 지정
+  	    response.setContentType("ms-vnd/excel");
+  	    response.setHeader("Content-Disposition", "attachment;filename=사원근태목록.xls");
+
+  	    // 엑셀 출력
+  	    wb.write(response.getOutputStream());
+  	    wb.close();
+
+  	}
+
 	
 }
